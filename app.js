ed569594404810d4eb0f64368666a296de75bfa4
Ahmad Zedany <zedany7@hotmail.com>
2017-02-23 23:04:08 +0200

    add client

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public/app')));
-
+ //
 
 app.use('/', index);
 app.use('/users', users);
