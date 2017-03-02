59f742d4c21681cdfd11bd1550318f26d5592392
kholodRobo <kholod@robohold.com>
2017-03-02 14:20:14 +0200

    changes updates part 2

--- public/app/services/forms_service.js
+++ public/app/services/forms_service.js
@@ -19,7 +19,9 @@ function ConfigurationService($state) {
 function FormDetailsService($http, $q) {
 
     var apiBase='https://townoffices.herokuapp.com/api/';
-   /* var  apiBase= 'http://localhost:3000/api/';*/
+
+    /* var  apiBase= 'http://localhost:3000/api/';*/
+    /* var  apiBase= 'http://localhost:3000/api/';*/
   return ({
     create: create,
     edit:edit,
